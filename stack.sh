c134182c20e927097cbf21ee20093cd993c4178d
root <root@control>
2013-07-04 13:52:29 +0900

    first

--- stack.sh
+++ stack.sh
@@ -992,7 +992,7 @@ if is_service_enabled q-svc; then
     echo_summary "Starting Quantum"
 
     start_quantum_service_and_check
-    create_quantum_initial_network
+    #create_quantum_initial_network
     setup_quantum_debug
 elif is_service_enabled $DATABASE_BACKENDS && is_service_enabled n-net; then
     # Create a small network
