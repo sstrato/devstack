e243e24a1e83f4d258afbad1b27c606d2541c5f7
root <root@compute2>
2013-07-12 05:27:59 +0000

    revert script added

--- post-script.sh
+++ post-script.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
-quantum net-create private-net-01 --provider:network_type flat --provider:physical_network private-net-01 --shared
-quantum subnet-create private-net-01 172.27.0.0/24 --name private-subnet-01
-quantum subnet-update private-subnet-01 --dns_nameservers list=true 8.8.8.8
-quantum router-create private-net-01-router
-quantum router-interface-add private-net-01-router private-subnet-01
+quantum net-create multihost-net-01 --provider:network_type flat --provider:physical_network private-net-01 --shared
+quantum subnet-create multihost-net-01 172.27.0.0/24 --name multihost-subnet-01
+quantum subnet-update multihost-subnet-01 --dns_nameservers list=true 8.8.8.8
+quantum router-create multihost-01-router
+quantum router-interface-add multihost-01-router multihost-subnet-01
 quantum net-create floating-net --router:external=True
 quantum subnet-create floating-net 172.16.0.100/32
 quantum subnet-create floating-net 172.16.0.101/32
 quantum subnet-create floating-net 172.16.0.102/32
 quantum subnet-create floating-net 172.16.0.103/32
 quantum subnet-create floating-net 172.16.0.104/32
-quantum router-gateway-set private-net-01-router floating-net
+quantum router-gateway-set multihost-net-01-router floating-net
--- pre-physical_env.sh
+++ pre-physical_env.sh
@@ -1,9 +1,27 @@
 #clear up virtualbox default gw
-ip add flush eth0 
-ip addr flush eth1
+expected_args=1
 
-ovs-vsctl add-port br-ex eth1
-ip link set br-ex up
-ip addr add 10.1.1.201/24 dev br-ex
-route del default gw 10.0.2.15
-route add default gw 10.1.1.254 dev br-ex
+if [ $# -ne $expected_args ]
+then
+		echo " usage: $0 revert/do "
+		exit
+fi
+
+case $1 in
+		revert )
+				ip addr add 10.0.2.15/24 dev eth0     
+				route add default gw 10.0.2.2 dev eth0  
+				;;
+		do )
+
+				ifconfig eth0 down
+				ip add flush eth0 
+				ip addr flush eth1
+				ip addr flush br-ex
+				ovs-vsctl add-port br-ex eth1
+				ip link set br-ex up
+				ip addr add 10.1.1.204/24 dev br-ex
+				route del default gw 10.0.2.15
+				route add default gw 10.1.1.254 dev eth0
+esac
+
