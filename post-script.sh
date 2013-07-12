3187eac40bad19376f797343f5c0f65c1af54a09
root <root@compute1>
2013-07-12 07:35:13 +0000

    brigde multihost added

--- post-script.sh
+++ post-script.sh
@@ -1,14 +1,18 @@
 #! /usr/bin/env bash
 
-quantum net-create private-net-01 --provider:network_type flat --provider:physical_network private-net-01 --shared
-quantum subnet-create private-net-01 172.27.0.0/24 --name private-subnet-01
-quantum subnet-update private-subnet-01 --dns_nameservers list=true 8.8.8.8
-quantum router-create private-net-01-router
-quantum router-interface-add private-net-01-router private-subnet-01
+
+source openrc admin admin
+quantum net-create multihost-net-01 --multihost:multi_host --provider:network_type flat --provider:physical_network multihost-net-01 --shared
+NET_ID=$(quantum net-list | grep multihost-net-01 | get_field 1)
+quantum router-create multihost-router-01 --multihost:network_id $NET_ID
+quantum subnet-create multihost-net-01 172.27.0.0/24 --name multihost-subnet-01
+quantum subnet-update multihost-subnet-01 --dns_nameservers list=true 8.8.8.8
+quantum router-interface-add multihost-router-01 multihost-subnet-01
 quantum net-create floating-net --router:external=True
-quantum subnet-create floating-net 172.16.0.100/32
-quantum subnet-create floating-net 172.16.0.101/32
-quantum subnet-create floating-net 172.16.0.102/32
-quantum subnet-create floating-net 172.16.0.103/32
-quantum subnet-create floating-net 172.16.0.104/32
-quantum router-gateway-set private-net-01-router floating-net
+#quantum subnet-create floating-net 192.168.1.0/24 --name floating-subnet --gateway 192.168.1.254 --allocation-pool start=192.168.1.100,end=192.168.1.150 --disable-dhcp
+quantum subnet-create floating-net 10.1.1.100/32
+quantum subnet-create floating-net 10.1.1.101/32
+quantum subnet-create floating-net 10.1.1.102/32
+quantum subnet-create floating-net 10.1.1.103/32
+quantum subnet-create floating-net 10.1.1.104/32
+quantum router-gateway-set multihost-router-01 floating-net
--- pre-physical_env.sh
+++ pre-physical_env.sh
@@ -1,9 +1,11 @@
 #clear up virtualbox default gw
 ip add flush eth0 
 ip addr flush eth1
+ip add flush br-ex
+
 
 ovs-vsctl add-port br-ex eth1
 ip link set br-ex up
-ip addr add 10.1.1.201/24 dev br-ex
+ip addr add 10.1.1.203/24 dev br-ex
 route del default gw 10.0.2.15
 route add default gw 10.1.1.254 dev br-ex
